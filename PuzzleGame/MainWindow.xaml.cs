b959d9a2279de187f5babc1312da3014d3ee872e
Jundos <belkakazmirov@gmail.com>
2017-11-04 20:47:37 +0200

    2 commit

--- PuzzleGame/MainWindow.xaml.cs
+++ PuzzleGame/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Windows;
@@ -19,9 +20,54 @@ namespace PuzzleGame
     /// </summary>
     public partial class MainWindow : Window
     {
+        //ObservableCollection<ColumnDefinition> coldef = new ObservableCollection<ColumnDefinition>();
+        //ObservableCollection<RowDefinition> rowdef = new ObservableCollection<RowDefinition>();
+        protected void CreateColandRowinPole(int Col,int Row)
+        {   
+            for (int i = 0; i < Col; i++)
+            {
+
+                Pole.ColumnDefinitions.Add(new ColumnDefinition()
+                {
+                    Width = new GridLength(1, GridUnitType.Star)
+                });
+            }
+
+            for (int i = 0; i < Row; i++)
+            {
+                Pole.RowDefinitions.Add(new RowDefinition()
+                {
+                    Height = new GridLength(120)
+                });
+            }
+        }
+
         public MainWindow()
         {
             InitializeComponent();
+
+            CreateColandRowinPole(3, 3);
+
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    Rectangle rect = new Rectangle
+                    {
+                        Width = 100,
+                        Height = 100,
+                        Fill = new SolidColorBrush(Colors.BlueViolet),
+                        Stroke = new SolidColorBrush(Colors.Black),
+                        StrokeThickness = 2,
+                        Margin = new Thickness(5)
+                    };
+                    rect.SetValue(Grid.RowProperty, i);
+                    rect.SetValue(Grid.ColumnProperty, j);
+                    Pole.Children.Add(rect);
+                }
+                
+            }
+
         }
     }
 }
